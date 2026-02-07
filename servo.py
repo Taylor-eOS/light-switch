14dba2e6f17919b7620a8265a2dbab110975364e
Taylor-eOS <taylor.eos@e.email>
2026-02-07 09:24:20 +0100

    RP2040 RGB LED blink

--- servo.py
+++ servo.py
@@ -1,16 +1,31 @@
 from machine import Pin, PWM
 import time
+import neopixel
 
 servo_pin = 14
+led_pin = 16
+
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
+
+np = neopixel.NeoPixel(Pin(led_pin), 1)
+
+def blink_status():
+    np[0] = (0, 40, 0)
+    np.write()
+    time.sleep(0.08)
+    np[0] = (0, 0, 0)
+    np.write()
+    time.sleep(0.12)
 
 def set_angle(pulse_us):
     period_us = 20000
     duty = int(pulse_us * 65535 // period_us)
     pwm.duty_u16(duty)
+    blink_status()
 
-set_angle(1688);
-time.sleep(2)
-set_angle(1792);
-
+set_angle(1688)
+time.sleep(0.1)
+set_angle(1792)
+time.sleep(1.8)
+set_angle(1688)
