5ef6ef2192be347c48f371136309b6059737c0e4
Taylor-eOS <taylor.eos@e.email>
2026-02-07 08:18:24 +0100

    Angle test

--- servo.py
+++ servo.py
@@ -5,24 +5,12 @@ servo_pin = 14
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
 
-def set_angle_us(pulse_us):
+def set_angle(pulse_us):
     period_us = 20000
     duty = int(pulse_us * 65535 // period_us)
     pwm.duty_u16(duty)
 
-#one end
-set_angle_us(500)
-time.sleep(1)
+set_angle(1790);
+time.sleep(2)
+set_angle(1660);
 
-set_angle_us(1000)
-time.sleep(1)
-
-set_angle_us(1500)
-time.sleep(1)
-
-set_angle_us(2000)
-time.sleep(1)
-
-#other end
-set_angle_us(2500)
-time.sleep(1)
